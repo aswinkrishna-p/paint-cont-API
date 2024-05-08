92cdbac5d3c2953647eee250ef3281ae1e00208b
aswin krishna <aswinkrishna33716@gmail.com>
2024-05-08 23:39:14 +0530

    post setups

---
+++ src/entity/PostEntity.ts
@@ -0,0 +1,10 @@
+
+export interface Ipost{
+    painterId: string;
+    media: string;
+    description: string;
+    comments: string[];
+    time: Date;
+    likes: number;
+    reportCount: number;
+}
