a6b7fa9ce5deac81293752870e35b7ecf4782018
aswin krishna <aswinkrishna33716@gmail.com>
2024-05-29 23:29:56 +0530

    made changes in this repo

--- src/frameworks/database/repository/post-repository.ts
+++ src/frameworks/database/repository/post-repository.ts
@@ -1,53 +1,45 @@
 import PostModel from "../models/postModel";
 
-
-
-
-
 class postRepository {
-
-
-    async allPosts(){
-        try {
-           
-           const allposts = await PostModel.find().populate("painterId")
-           // console.log(allposts,'all postss ');
-           return allposts
-        } catch (error) {
-           console.log(error);
-           
-        }
-      }
-
-      async reportPost(postId:string){
-         try {
-            
-            const post = await PostModel.findByIdAndUpdate(postId, { $inc: { reportCount: 1 } }, { new: true });
-
-            
-    if (!post) {
-      return {
-         success:false,
-         message:'post not found'
-      }
-    }else if(post.reportCount === 1){
-      return {
-         success:true,
-         message:'Report count updated successfully',
-         reportLimitReached:true
-      }
-    }else{
-      return {
-         success:true,
-         message:'Report count updated ...'
-      }
+  async allPosts() {
+    try {
+      const allposts = await PostModel.find().populate("painterId");
+      // console.log(allposts,'all postss ');
+      return allposts;
+    } catch (error) {
+      console.log(error);
     }
-         } catch (error) {
-            console.log(error);
-            
-         }
+  }
+
+  async reportPost(postId: string) {
+    try {
+      const post = await PostModel.findByIdAndUpdate(
+        postId,
+        { $inc: { reportCount: 1 } },
+        { new: true }
+      );
+
+      if (!post) {
+        return {
+          success: false,
+          message: "post not found",
+        };
+      } else if (post.reportCount === 1) {
+        return {
+          success: true,
+          message: "Report count updated successfully",
+          reportLimitReached: true,
+        };
+      } else {
+        return {
+          success: true,
+          message: "Report count updated ...",
+        };
       }
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
 
-
-export default postRepository
+export default postRepository;
