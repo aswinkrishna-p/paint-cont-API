2440b475af53c1ef368983d5ab49c67590e41634
aswin krishna <aswinkrishna33716@gmail.com>
2024-06-28 23:22:34 +0530

    integrating stripe

---
+++ src/frameworks/services/stripe.ts
@@ -0,0 +1,5 @@
+import Stripe from "stripe";
+
+const stripe = new Stripe('pk_test_51PWM0509AUT7QrJpLv97qkMWc3ekvZxV3v03Yy5TsW1PFKAI4qjn5Kw2eIBQAE6O0LvbVn8JQ3Jyu2R0ksjW7xF700oGykZZfF')
+
+export default stripe
